99c3195c71f503ec1d961d353e8e471498d4f68c
Jeffrey Pfau <jeffrey@endrift.com>
2015-01-06 20:50:19 -0800

    Add bug tag

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -11,6 +11,19 @@ module Jekyll
             "<img class=\"hero\" src=\"#{baseurl}/resources/post-assets/#{@image}\" alt=\"#{@alt}\">"
         end
     end
+
+	class BugTag < Liquid::Tag
+        def initialize(tag_name, text, tokens)
+            super
+            @id = text.strip
+        end
+
+        def render(context)
+            baseurl = context.registers[:site].config['baseurl']
+            "<a href=\"#{baseurl}/bugs/show_bug.cgi?id=#{@id}\">##{@id}</a>"
+        end
+	end
 end
 
 Liquid::Template.register_tag('hero', Jekyll::HeroTag)
+Liquid::Template.register_tag('bug', Jekyll::BugTag)
